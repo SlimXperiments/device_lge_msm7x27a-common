122974618658c8fda69d68741a94ea1ea1fd73b8
Anthony King <anthonydking@gmail.com>
2014-03-02 15:59:01 +0000

    msm7x27a-common: add in audio_effects for soundfx and DSP
    Change-Id: I23fe90801288aca1d99de7e308d74591fc8a506c

--- msm7x27a-common.mk
+++ msm7x27a-common.mk
@@ -13,7 +13,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
-    $(LOCAL_PATH)/configs/audio_policy.conf::system/etc/audio_policy.conf
+    $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/configs/audio_effects.conf:system/vendor/etc/audio_effects.conf
 
 
 $(call inherit-product, build/target/product/full.mk)
